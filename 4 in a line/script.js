931f2e6ae6c2dbb6fdac99e5852e697738ecf2b1
Yogu Wong <104674277+yogu13579@users.noreply.github.com>
2024-08-24 16:31:31 +0800

    add 2 games

---
+++ 4 in a line/script.js
@@ -0,0 +1,32 @@
+document.addEventListener("mousemove", getmouse);
+document.addEventListener("click", function (event) {
+  console.log(event.button);
+});
+const playBoard = document.querySelector(".play-board");
+
+function getmouse(event) {
+  const rect = playBoard.getBoundingClientRect();
+
+  const realWidth = rect.width / 8;
+  const realHight = rect.height / 5;
+
+  const mouseX = event.clientX - rect.left;
+  const mouseY = event.clientY - rect.top;
+
+  let x = Math.floor(mouseX / realWidth);
+  let y = Math.floor(mouseY / realHight);
+
+  if (y < 0) {
+    return 1;
+  } else if (x > 7) {
+    return 1;
+  } else if (y > 4) {
+    return 1;
+  } else if (x < 0) {
+    return 1;
+  } else {
+    console.log(`X : ${x}, Y : ${y}`);
+    let html = `<div class="piece" style="grid-area: ${y + 1} / ${x + 1}"></div>`;
+    playBoard.innerHTML = html;
+  }
+}
