bf8917a3092d01658d7ff54bad8c9f78518ea853
Yogu Wong <104674277+yogu13579@users.noreply.github.com>
2024-08-26 07:55:49 +0800

    making 4 in a line game

--- 4 in a line/script.js
+++ 4 in a line/script.js
@@ -1,8 +1,12 @@
 const playBoard = document.querySelector(".play-board");
+const line = document.querySelector(".line");
+const who = document.querySelector(".turn");
+let color;
+let lx;
+let ly;
+let turn = 2;
 
-document.addEventListener("mousemove", getmouse);
-
-function getmouse(event) {
+document.addEventListener("mousemove", function (event) {
   const rect = playBoard.getBoundingClientRect();
 
   const realWidth = rect.width / 8;
@@ -11,20 +15,49 @@ function getmouse(event) {
   const mouseX = event.clientX - rect.left;
   const mouseY = event.clientY - rect.top;
 
-  let x = Math.floor(mouseX / realWidth);
-  let y = Math.floor(mouseY / realHight);
+  x = Math.floor(mouseX / realWidth);
+  y = Math.floor(mouseY / realHight);
 
   if (y < 0) {
-    return 1;
+    return;
   } else if (x > 7) {
-    return 1;
+    return;
   } else if (y > 4) {
-    return 1;
+    return;
   } else if (x < 0) {
-    return 1;
+    return;
   } else {
     console.log(`X : ${x}, Y : ${y}`);
-    let html = `<div class="piece" style="grid-area: ${y + 1} / ${x + 1}"></div>`;
-    playBoard.innerHTML = html;
+    let html = `<div class="piece" style="grid-area: ${y + 1} / ${x + 1}"></div>`
+    // playBoard.insertAdjacentHTML("beforeend", html)
+    line.innerText = `It's line ${x + 1}`
+    lx = x;
+    ly = y;
   }
-}
+  
+});
+
+document.addEventListener("click", function (event){ 
+  
+  if (turn % 2 === 0) {
+    if (event.target.style.backgroundColor === "white") {
+      event.target.style.backgroundColor = 'red';
+      turn++
+      who.innerText = `Who's turn : blue`
+    } else {
+      return;
+    }
+  } else if (turn % 2 !== 0) {
+    if (event.target.style.backgroundColor === "white") {
+      event.target.style.backgroundColor = 'blue';
+      turn++
+      who.innerText = `Who's turn : red`
+    } else {
+      return;
+    }
+  }
+});
+
+
+
+
