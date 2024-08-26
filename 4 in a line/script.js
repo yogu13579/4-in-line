e85203a60ee025c0b6aa1484b8d274884a871bfb
Yogu Wong <104674277+yogu13579@users.noreply.github.com>
2024-08-26 20:36:27 +0800

    Update script.js

--- 4 in a line/script.js
+++ 4 in a line/script.js
@@ -1,6 +1,7 @@
 const playBoard = document.querySelector(".play-board");
 const line = document.querySelector(".line");
 const who = document.querySelector(".turn");
+let setIntervalId;
 let color;
 let lx;
 let ly;
@@ -17,6 +18,67 @@ let okY5 = 4;
 let okY6 = 4;
 let okY7 = 4;
 let okY8 = 4;
+let winCards1 = ['0o4','14','24','34']
+let winCards2 = ['14','24','34','44']
+let winCards3 = ['24','34','44','54']
+let winCards4 = ['34','44','54','54']
+let winCards5 = ['44','54','64','74']
+let winCards6 = ['0o3','13','23','33']
+let winCards7 = ['13','23','33','43']
+let winCards8 = ['23','33','43','53']
+let winCards9 = ['33','43','53','53']
+let winCards10 = ['43','53','63','73']
+let winCards11 = ['0o2','12','22','32']
+let winCards12 = ['12','22','32','42']
+let winCards13 = ['22','32','42','52']
+let winCards14 = ['32','42','52','52']
+let winCards15 = ['42','52','62','72']
+let winCards16 = ['0o1','11','21','31']
+let winCards17 = ['11','21','31','41']
+let winCards18 = ['21','31','41','51']
+let winCards19 = ['31','41','51','51']
+let winCards20 = ['41','51','61','71']
+let winCards21 = ['0o0','10','20','30']
+let winCards22 = ['10','20','30','40']
+let winCards23 = ['20','30','40','50']
+let winCards24 = ['30','40','50','50']
+let winCards25 = ['40','50','60','70']
+let winCards26 = ['0o0','0o1','0o2','0o3']
+let winCards27 = ['0o1','0o2','0o3','0o4']
+let winCards28 = ['10','11','12','13']
+let winCards29 = ['11','12','13','14']
+let winCards30 = ['20','21','22','23']
+let winCards31 = ['21','22','23','24']
+let winCards32 = ['30','31','32','33']
+let winCards33 = ['31','32','33','34']
+let winCards34 = ['40','41','42','43']
+let winCards35 = ['41','42','43','44']
+let winCards36 = ['50','51','52','53']
+let winCards37 = ['51','52','53','54']
+let winCards38 = ['60','61','62','63']
+let winCards39 = ['61','62','63','64']
+let winCards40 = ['70','71','72','73']
+let winCards41 = ['71','72','73','74']
+let winCards42 = ['0o4','13','22','31']
+let winCards43 = ['14','23','32','41']
+let winCards44 = ['24','33','42','51']
+let winCards45 = ['34','43','52','61']
+let winCards46 = ['44','53','62','62']
+let winCards47 = ['0o3','12','21','30']
+let winCards48 = ['13','22','31','40']
+let winCards49 = ['23','32','41','50']
+let winCards50 = ['33','42','51','60']
+let winCards51 = ['43','52','61','70']
+let winCards52 = ['74','63','52','41']
+let winCards53 = ['64','53','42','31']
+let winCards54 = ['54','43','32','21']
+let winCards55 = ['44','33','22','11']
+let winCards56 = ['34','23','12','0o1']
+let winCards57 = ['73','62','51','40']
+let winCards58 = ['63','52','41','30']
+let winCards59 = ['53','42','31','20']
+let winCards60 = ['43','32','21','10']
+let winCards61 = ['33','22','11','0o0']
 
 document.addEventListener("mousemove", function (event) {
   const rect = playBoard.getBoundingClientRect();
@@ -49,6 +111,12 @@ document.addEventListener("mousemove", function (event) {
   
 });
 
+function redwins() {
+  clearInterval(setIntervalId);
+  alert("Game Over! Red is the winner! Press OK to replay...");
+  location.reload();
+}
+
 document.addEventListener("click", function (event){ 
   
   if (ly === okY1 && lx === 0) {
@@ -60,6 +128,7 @@ document.addEventListener("click", function (event){
         okY1--;
         who.innerText = `Who's turn : blue`;
         i++;
+        
       } else {
         return;
       }
@@ -243,12 +312,16 @@ document.addEventListener("click", function (event){
       }
     }
   }
-  console.log(red);
-  console.log(blue);
+
+  
 });
 
+function a() {
+  console.log(red);
+  console.log(blue);
+}
 
-
+setIntervalId = setInterval(a, 100000);
 
 
 
